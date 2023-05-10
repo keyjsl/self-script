163d345fdc383ffaff0d2bccb7a574cd1173d1dc
keyjsl <111936411+keyjsl@users.noreply.github.com>
2023-05-10 21:49:27 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -13,10 +13,35 @@ cd /root/cloudflare-ddns-updater
 echo "Wait for 5 seconds"
 sleep 5
 
+echo “Function to check if IP address belongs to Cloudflare”
+ip_belongs_to_cloudflare() {
+    local ip_address=$(curl -sS https://api.ipify.org)
+    if [[ $ip_address == *"Cloudflare"* ]]; then
+        return 0  # IP address belongs to Cloudflare
+    else
+        return 1  # IP address does not belong to Cloudflare
+    fi
+}
+
 echo "Running warp-go o in root"
 echo "0" | warp-go o
 
-echo "Update crontab entry to schedule script every hour"
-(crontab -l ; echo "0 * * * * /root/ddns.sh") | crontab -
+echo "Checking IP address for Cloudflare"
+while ! ip_belongs_to_cloudflare; do
+    echo "IP address does not belong to Cloudflare, running warp-go o in root again"
+    echo "0" | warp-go o
+    sleep 5  echo "Wait for 5 seconds"
+done
+
+echo "IP address belongs to Cloudflare"
+
+echo "Check if crontab entry already exists"
+if crontab -l | grep -q "/root/ddns.sh"; then
+    echo "Crontab entry already exists"
+else
+    # Update crontab entry to schedule script every hour
+    (crontab -l ; echo "0 * * * * /root/ddns.sh") | crontab -
+    echo "Crontab entry added"
+fi
 
 echo "Script execution completed"
